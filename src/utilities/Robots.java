015f03f728bdede6685f725d0d075600a4ca7787
Anaïs Donzeau <anais.donzeau@ensimag.grenoble-inp.fr>
2016-10-14 18:59:58 +0200

    Initial pour de vrai

---
+++ src/utilities/Robots.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+/**
+ *
+ * @author skip
+ */
+public class Robots {
+    protected int vitesse;
+    protected case position; 
+    
+    public case getPosition(){
+        return position;
+    }
+    
+    public void setPosition(case p){
+        this.position=p;
+    }
+    
+    
+    
+    
+}
