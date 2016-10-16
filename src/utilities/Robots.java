dd2bb09c1033e2799e6009557804e371122547d7
Anaïs Donzeau <anais.donzeau@ensimag.grenoble-inp.fr>
2016-10-16 15:10:53 +0200

    Classe robots en construction

--- src/utilities/Robots.java
+++ src/utilities/Robots.java
@@ -10,18 +10,41 @@ package utilities;
  * @author skip
  */
 public class Robots {
-    /*protected int vitesse;
-    protected case position; 
     
-    public case getPosition(){
-        return position;
-    }
-    
-    public void setPosition(case p){
-        this.position=p;
-    }
-    */
-    
-    
-    
+//   protected double vitesseDefault;
+//    
+//    protected case position; 
+//    
+//    protected int capaciteMax;
+//    
+//    protected int volumeRestant;
+//    
+//
+//    
+//    public case getPosition(){
+//        return position;
+//    }
+//    
+//    public void setPosition(case p){
+//        this.position=p;
+//    }
+//    
+//    public double getVitesse(NatureTerrain nature){
+//        return vitesse_default;
+//    }
+//    
+//    public void remplirReservoir(){
+//        this.volumeRestant=this.capaciteMax;        
+//    }
+//    
+//    public void deverserEau(int vol){
+//        if (vol>=this.volumeRestant) {
+//            this.volumeRestant -= vol;
+//        } else {
+//            throw new IllegalArgumentException("Volume supérieur au volume restant");
+//        }
+//        
+//        /* à ajouter quand besoin de répercuter sur le feu*/
+//    }
+
 }
