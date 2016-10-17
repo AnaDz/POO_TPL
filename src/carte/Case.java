d3d8c5fbde55085d3300607ddf09e84d97823a49
Lavechin Marvin <marvin.lavechin@ensimag.grenoble-inp.fr>
2016-10-17 16:14:48 +0200

    Correction du package carte

---
+++ src/carte/Case.java
@@ -0,0 +1,27 @@
+package carte;
+
+public class Case {
+	private int ligne;
+	private int colonne;
+	private NatureTerrain nature;
+	
+	
+	public Case(int l, int c, NatureTerrain n){
+		ligne = l;
+		colonne = c;
+		nature = n;
+	}
+	
+	public int getLigne(){
+		return ligne;
+	}
+	
+	public int getColonne(){
+		return colonne;
+	}
+	
+	public NatureTerrain getNatureTerrain(){
+		return nature;
+	}
+	
+}
