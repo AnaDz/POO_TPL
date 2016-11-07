b6d4e1ff93741f453aaee3aff58e7d2a340b4b72
Lavechin Marvin <marvin.lavechin@ensimag.grenoble-inp.fr>
2016-11-07 01:34:12 +0100

    Ajout du test associé à l'algo du plus court chemin

--- src/TestPlusCourtChemin.java
+++ src/TestPlusCourtChemin.java
@@ -1,20 +1,21 @@
 import java.awt.Color;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.zip.DataFormatException;
 
+import carte.Case;
 import carte.Direction;
 import gui.GUISimulator;
 import interfacegraphique.Simulateur;
 import io.LecteurDonnees;
 import robots.Robot;
 import donneesSimulation.DonneesSimulation;
-import evenements.DeplaceRobot;
-import evenements.Evenement;
-import evenements.GestionnaireEvents;
-import evenements.ProgrammeFin;
+import evenements.*;
 import carte.*;
 import strategie.*;
-
+/**
+ * Ce fichier teste l'algorithme de plus court chemin. Il est a appeler avec cartes/cartePlusCourtChemin.map
+ */
 public class TestPlusCourtChemin {
 
 	public static void main(String[] args) {
@@ -27,12 +28,40 @@ public class TestPlusCourtChemin {
         	//Lecture des Données
             DonneesSimulation data = LecteurDonnees.lire(args[0], false);
             Robot.setDS(data);
+            
+            //Calcul des plus courts chemins
             Carte carte = data.getCarte();
             Robot rob = data.getListeRobots().get(0);
             Case deb = rob.getPosition();
-            Case fin = carte.getCase(carte.getNbLignes()-1, 0);
-            AstarPlusCourtChemin ASPCC = new AstarPlusCourtChemin(carte, rob, deb, fin);
-            System.out.println(ASPCC.trouveChemin());
+            Case fin = carte.getCase(7, 3);
+            List<Case> C0 = AStar.trouveChemin(carte, rob, deb, fin);
+            System.out.println("Chemin du robot 0 : "+C0);
+            
+            rob = data.getListeRobots().get(1);
+            deb = rob.getPosition();
+            List<Case> C1 = AStar.trouveChemin(carte, rob, deb, fin);
+            System.out.println("Chemin du robot 1 : "+C1);
+            
+            rob = data.getListeRobots().get(2);
+            deb = rob.getPosition();
+            List<Case> C2 = AStar.trouveChemin(carte, rob, deb, fin);
+            System.out.println("Chemin du robot 2 : "+C2);
+            
+            rob = data.getListeRobots().get(3);
+            deb = rob.getPosition();
+            List<Case> C3 = AStar.trouveChemin(carte, rob, deb, fin);
+            System.out.println("Chemin du robot 3 : "+C3);
+            
+            
+            //TO DO
+            //Traduire ça en successions de déplacements. Les robots doivent tous arriver à la case (7,3)
+            GestionnaireEvents GE = new GestionnaireEvents();
+            
+            //Lancement de la simulation
+            int dimFenX = 500;
+            int dimFenY = 500;
+            GUISimulator gui = new GUISimulator(dimFenX, dimFenY, Color.BLACK);
+            Simulateur sim = new Simulateur(gui, data, GE);
      
         } catch (FileNotFoundException e) {
             System.out.println("fichier " + args[0] + " inconnu ou illisible");
---
+++ src/strategie/AStar.java
@@ -0,0 +1,138 @@
+package strategie;
+import java.util.*;
+import robots.Robot;
+import carte.*;
+import java.util.PriorityQueue;
+/** 
+ * Cette classe statique implémente l'algorithme de A* de recherche de plus court chemin.
+ * Vous trouverez une très bonne description de l'algorithme sur la page anglophone associée de Wikipédia :
+ * https://en.wikipedia.org/wiki/A*_search_algorithm
+ */
+public class AStar {
+	
+	/** Le tableau de f et h
+	 *  Voir description de l'algorithme de A*
+	 */
+	static double [][][] couts;
+	
+	/**
+	 * Cette fonction trouve le plus court chemin grâce à l'algorithme de A*.
+	 * @param carte	la carte dans laquelle le robot évolue
+	 * @param rob	le robot qui doit se déplacer
+	 * @param deb	la case d'où part le robot
+	 * @param fin	la case où doit arriver le robot
+	 * @return		le plus court chemin de deb à fin
+	 */
+	public static List<Case> trouveChemin(Carte carte, Robot rob, Case deb, Case fin){
+		/** L'ensemble des noeuds qui restent à explorer **/
+		PriorityQueue<Case> openQueue = new PriorityQueue<Case> (10, new NodeComparator());
+		/** L'ensemble des noeuds qui ont été exploré **/
+		Set<Case> closedList = new HashSet<Case>();
+		/** La variable destiné à contenir le chemin **/
+	    Map<Case, Case> path = new HashMap<Case, Case>();
+		
+	    /** INITIALISATION **/
+	    couts = new double [carte.getNbLignes()][carte.getNbColonnes()][2];
+	    for(int i = 0; i < carte.getNbLignes(); i++) {
+	    	for(int j = 0; j < carte.getNbColonnes(); j++){
+	    		couts[i][j][0] = Double.POSITIVE_INFINITY;
+ 	    	}
+	    }
+	    /** Algorithme A* **/
+	    couts[deb.getLigne()][deb.getColonne()][0] = 0;
+	    couts[deb.getLigne()][deb.getColonne()][1] = 0 + heuristique(deb,fin);
+		openQueue.add(deb);
+		while(!openQueue.isEmpty()) {
+			Case u = openQueue.poll(); //on dépile u
+			if(u == fin) {
+				for(int i = 0; i < carte.getNbLignes(); i++){
+					for(int j = 0; j < carte.getNbColonnes(); j++) {
+						System.out.print(couts[i][j][0] +"\t");
+					}
+					System.out.print("\n");
+				}
+				return path(path, fin);
+			}
+			closedList.add(u);
+			ArrayList<Case> voisin = carte.getAllVoisins(u);
+			
+			for(Case v : voisin) {
+				//On ignore les voisins déjà évalués et les cases où le robot ne peut pas aller
+				double vitesse = rob.getVitesse(v.getNatureTerrain());
+				if(vitesse == 0 || closedList.contains(v)) continue; 
+				
+				//La distance du début jusqu'au voisin.
+				double distanceBetweenUandV = rob.getDureeDeplacement(0.5, u);
+				double tentativeG = distanceBetweenUandV + couts[u.getLigne()][u.getColonne()][0];
+				
+				if(!openQueue.contains(v))
+					openQueue.add(v);
+				else if (tentativeG >= couts[v.getLigne()][v.getColonne()][0])
+					continue; //ce n'est pas un meilleur chemin
+				
+				//C'est le meilleur chemin jusqu'à présent. On le sauvegarde
+				//On modifie le f de v
+				//On est donc forcés de le remove, pour pouvoir l'add ensuite
+				//afin de conserver la propriété de la priorityQueue
+				openQueue.remove(v);
+				couts[v.getLigne()][v.getColonne()][0] = tentativeG;
+				couts[v.getLigne()][v.getColonne()][1] = tentativeG + heuristique(v, fin);
+				openQueue.add(v);
+		
+				path.put(v, u);
+				
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Calcule l'heuristique par rapport à la destination utilisée dans l'algorithme de A*.
+	 * Ici on utilise la distance de Manhattan en guise d'heuristique.
+	 * @param x		le noeud dont on doit calculer l'heuristique
+	 * @param fin	le noeud destination
+	 * @return		l'heuristique associé au noeud x
+	 */
+	private static double heuristique(Case x, Case fin) {
+		return Math.abs(fin.getLigne()-x.getLigne())+ Math.abs(fin.getColonne()-x.getColonne());
+	}
+	/**
+	 * Cette fonction retourne le chemin que le robot doit parcourir en remontant la liste retournée par A*.
+	 * @param path			le chemin retourné par la fonction trouveChemin
+	 * @param destination	la destination finale du robot, qui est aussi la dernière case visité par l'algorithme de A*
+	 * @return				la suite des cases que le chemin doit parcourir de la position deb à la position fin
+	 */
+	private static List<Case> path (Map<Case,Case> path, Case destination) {
+		assert path != null;
+        assert destination != null;
+
+        List<Case> pathList = new ArrayList<Case>();
+        pathList.add(destination);
+        while (path.containsKey(destination)) {
+            destination = path.get(destination);
+            pathList.add(destination);
+        }
+        Collections.reverse(pathList);
+        return pathList;
+	}
+	
+	/**
+	 * Cette fonction implémente l'interface Comparator<Case> dans le but d'utiliser une PriorityQueue sur les cases.
+	 * Cette PriorityQueue trie les cases en fonction de leur cout f = g + h (:= la distance + l'heuristique).
+	 */
+	private static class NodeComparator implements Comparator<Case> {
+		public int compare(Case x, Case y) {
+			double f1 = couts[x.getLigne()][x.getColonne()][1];
+			double f2 = couts[y.getLigne()][y.getColonne()][1];
+	        if(f1 < f2) {
+	       	 return -1;
+	        } else if (f1 == f2) {
+	       	 return 0;
+	        } else {
+	       	 return 1;
+	        }
+		}
+	}
+	
+}
+
